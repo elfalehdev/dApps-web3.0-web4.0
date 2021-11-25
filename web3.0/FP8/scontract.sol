045f66c69e157fc9aaea5893c44ecd97c7ff507a
Mohamed Elfaleh <mohamedelfaleh5@gmail.com>
2021-11-25 11:52:43 +0100

    Update scontract.sol

--- web3.0/FP8/scontract.sol
+++ web3.0/FP8/scontract.sol
@@ -1,1 +1,8 @@
-/// lets just start this projects right away.
+pragma solidity ^0.4.4;
+
+library ConvertLib{
+	function convert(uint amount,uint conversionRate) returns (uint convertedAmount)
+	{
+		return amount * conversionRate;
+	}
+}
